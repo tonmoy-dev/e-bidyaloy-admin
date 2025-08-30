885007df9abace7ef3af82a02b150049ef58c90d
tonmoy-dev <rxdevs100@gmail.com>
2025-08-30 11:30:22 +0600

    update table column actions for table pages

--- src/shared/components/table/DataTableColumnActions.tsx
+++ src/shared/components/table/DataTableColumnActions.tsx
@@ -1,53 +1,57 @@
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
 export default function DataTableColumnActions({
+  showViewAction = true,
+  showEditAction = true,
+  showDeleteAction = true,
   viewActionId,
   editActionId,
 }: {
+  showViewAction?: boolean;
+  showEditAction?: boolean;
+  showDeleteAction?: boolean;
   viewActionId?: string;
   editActionId?: string;
 }) {
   return (
-    <div className="d-flex align-items-center">
-      <div className="dropdown">
-        <button
-          className="btn btn-white btn-icon btn-sm d-flex align-items-center justify-content-center rounded-circle p-0"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i className="ti ti-dots-vertical fs-14" />
-        </button>
-        <ul className="dropdown-menu dropdown-menu-right p-3">
-          <li>
-            <button
-              className="dropdown-item rounded-1"
-              data-bs-toggle="modal"
-              data-bs-target={`#${viewActionId}`}
-            >
-              <i className="ti ti-eye me-2" />
-              View
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item rounded-1"
-              data-bs-toggle="modal"
-              data-bs-target={`#${editActionId}`}
-            >
-              <i className="ti ti-edit-circle me-2" />
-              Edit
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item rounded-1"
-              data-bs-toggle="modal"
-              data-bs-target="#delete-modal"
-            >
-              <i className="ti ti-trash-x me-2" />
-              Delete
-            </button>
-          </li>
-        </ul>
-      </div>
+    <div className="d-flex align-items-center gap-2">
+      {showViewAction && viewActionId && (
+        <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-top">View</Tooltip>}>
+          <button
+            type="button"
+            className="btn btn-outline-light bg-white btn-icon me-1"
+            data-bs-toggle="modal"
+            data-bs-target={`#${viewActionId}`}
+          >
+            <i className="ti ti-eye" />
+          </button>
+        </OverlayTrigger>
+      )}
+      {showEditAction && editActionId && (
+        <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-top">Edit</Tooltip>}>
+          <button
+            type="button"
+            className="btn btn-outline-light bg-white btn-icon me-1"
+            data-bs-toggle="modal"
+            data-bs-target={`#${editActionId}`}
+          >
+            <i className="ti ti-edit-circle" />
+          </button>
+        </OverlayTrigger>
+      )}
+
+      {showDeleteAction && (
+        <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-top">Delete</Tooltip>}>
+          <button
+            type="button"
+            className="btn btn-outline-light bg-white btn-icon me-1"
+            data-bs-toggle="modal"
+            data-bs-target="#delete-modal"
+          >
+            <i className="ti ti-trash-x" />
+          </button>
+        </OverlayTrigger>
+      )}
     </div>
   );
 }
