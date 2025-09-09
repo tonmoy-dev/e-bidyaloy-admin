b5063cf7891585fc56b513087619ff165aa1e4b5
tonmoy-dev <rxdevs100@gmail.com>
2025-09-09 16:50:46 +0600

    chore: add api configs for netlify builds

--- src/core/constants/api.ts
+++ src/core/constants/api.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://212.28.189.126:8021';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://212.28.189.126:8021';
+
+// VITE_API_BASE_URL = http://212.28.189.126:8021
 
 export const API_ENDPOINTS = {
   AUTH: {
