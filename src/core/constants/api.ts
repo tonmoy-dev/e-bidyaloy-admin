bae4a0c0ad1c218867945bb0926da401201e9e0e
tonmoy-dev <rxdevs100@gmail.com>
2025-11-14 21:01:17 +0600

    update base api for server data

--- src/core/constants/api.ts
+++ src/core/constants/api.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'https://school.jordanbikestation.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://school.jordanbikestation.com';
 
 
 export const API_ENDPOINTS = {
